c9afc371c0463a7a18ceca523a22135e4975370d
Jannis Baum <jannis.baum@gmail.com>
2024-05-19 17:09:34 +0200

    feat: splice distributions seqname filtering

--- testing-data/splice_distributions.py
+++ testing-data/splice_distributions.py
@@ -4,32 +4,37 @@ import numpy as np
 import pandas as pd
 
 # helper to parse attributes and add them as a new column
-def _extract_attribute(df, attr):
+def _extract_attribute(df, attr, cast=None):
     def _parse_attribute(col, attr):
         # (some attributes are optional, that's why we need NaN)
         try: value = col.split(f'{attr} "')[1].split('";')[0]
         except: return np.NaN
 
-        try: return float(value)
-        except: return value
+        if not cast: return value
+        try: return cast(value)
+        except: return np.NaN
     df[attr] = df['attribute'].apply(_parse_attribute, args=(attr, ))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser('splice_distributions')
     parser.add_argument('gtf_in', help='Input GTF file from Stringtie')
+    parser.add_argument('-chr', '--chromosome', type=str, help='output only data for the given chromosome (or, more accurately, GTF seqname)')
     args = parser.parse_args()
 
     gtf_df = pd.read_csv(
-        args.gtf_in, sep="\t", comment='#', low_memory=False,
-        names=["seqname", "source", "feature", "start", "end", "score", "strand", "frame", "attribute"]
+        args.gtf_in, sep='\t', comment='#', low_memory=False,
+        names=['seqname', 'source', 'feature', 'start', 'end', 'score', 'strand', 'frame', 'attribute']
     )
+    if args.chromosome:
+        gtf_df = gtf_df[gtf_df.seqname == args.chromosome]
 
     # get transcripts
     transcripts = gtf_df[gtf_df['feature'] == 'transcript'].loc[:]
 
     # parse attributes
-    for attr in ['gene_id', 'transcript_id', 'FPKM']:
-        _extract_attribute(transcripts, attr)
+    _extract_attribute(transcripts, 'gene_id')
+    _extract_attribute(transcripts, 'transcript_id')
+    _extract_attribute(transcripts, 'FPKM', float)
     del transcripts['attribute']
 
     # get total FPKM score of gene (we would get the same result with TPM)
