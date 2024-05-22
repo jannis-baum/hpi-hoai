dabde3d7b77b7104dae3c8b119e9cb32b0053148
Jannis Baum <jannis.baum@gmail.com>
2024-05-22 13:59:13 +0200

    feat: get sequence from vcf file

--- testing-data/script/variants.py
+++ testing-data/script/variants.py
@@ -1,3 +1,4 @@
+from pyfaidx import Fasta
 from pysam import VariantFile
 
 def get_first_chromosome(vcf_path: str):
@@ -11,3 +12,20 @@ def filter_positions(vcf_in_path: str, vcf_out_path: str, whitelist: set[int]):
     for record in vcf_in:
         if record.pos in whitelist:
             vcf_out.write(record)
+
+class Annotator():
+    def __init__(self, fasta_path, vcf_path):
+        self._fasta = Fasta(fasta_path)
+        self._vcf = VariantFile(vcf_path)
+
+    def get_seq(self, chromosome, start, end):
+        ref_seq = list(self._fasta[chromosome][start:end].seq)
+
+        for record in self._vcf.fetch(chromosome, start, end):
+            pos = record.pos - 1 - start  # -1 for 0-based list index
+            ref = record.ref
+            alt = record.alts[0] # assume first alternative allele
+            # variant
+            ref_seq = ref_seq[:pos] + list(alt) + ref_seq[pos + len(ref):]
+
+        return ''.join(ref_seq)
