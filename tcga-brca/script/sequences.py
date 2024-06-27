4b3d41bd67309004a60f63e936a42ef19c2fc2da
Jannis Baum <jannis.baum@gmail.com>
2024-06-27 16:28:09 +0200

    feat: keep track of variant alignment

--- tcga-brca/script/sequences.py
+++ tcga-brca/script/sequences.py
@@ -9,17 +9,30 @@ class Annotator():
         self._fasta = Fasta(fasta_path)
         self._vcf = VariantFile(vcf_path)
 
-    def get_seq(self, chromosome: str, start: int, end: int) -> str:
-        ref_seq = list(self._fasta[chromosome][start:end].seq)
+    # get sequence with variants and reference indices for alignment:
+    # reference indices correspond to elements of sequence and denote which
+    # index/indices they correspond to in the reference genome
+    def get_seq(self, chromosome: str, start: int, end: int) -> tuple[str, list[list[int]]]:
+        seq = list(self._fasta[chromosome][start:end + 1].seq)
+        idx = [[i] for i in range(start, end + 1)]
 
-        for record in self._vcf.fetch(chromosome, start, end):
-            pos = record.pos - 1 - start  # -1 for 0-based list index
+        # sort records by position in descending order to not mess up indices
+        records = sorted(self._vcf.fetch(chromosome, start, end), key=lambda x: x.pos, reverse=True)
+        for record in records:
             ref = record.ref
             alt = record.alts[0] # assume first alternative allele
-            # variant
-            ref_seq = ref_seq[:pos] + list(alt) + ref_seq[pos + len(ref):]
 
-        return ''.join(ref_seq)
+            r_start = record.pos - 1 - start  # -1 for 0-based list index
+            r_end = r_start + len(ref)
 
-    def get_gene_seq(self, gene: Gene, padding: int = 5000) -> str:
+            # insert variance into sequence
+            seq = seq[:r_start] + list(alt) + seq[r_end:]
+            # keep track of corresponding indices
+            # (we have to reshift r_start/end by the original start position)
+            idx = idx[:r_start] + [[i for i in range(r_start + start, r_end + start)]] * len(alt) + idx[r_end:]
+
+        return (''.join(seq), idx)
+
+    def get_gene_seq(self, gene: Gene, padding: int = 5000) -> tuple[str, list[list[int]]]:
         return self.get_seq(gene.chromosome, gene.bp_start - padding, gene.bp_end + padding)
+
--- tcga-brca/script/splice_ai.py
+++ tcga-brca/script/splice_ai.py
@@ -6,6 +6,8 @@ from spliceai.utils import one_hot_encode
 _paths = ('models/spliceai{}.h5'.format(x) for x in range(1, 6))
 models = [load_model(resource_filename('spliceai', x)) for x in _paths]
 
+lost_padding = 5000
+
 def predict(seq: str) -> tuple[np.ndarray, np.ndarray]:
     encoded = one_hot_encode(seq)[None, :]
     y = np.mean([models[m].predict(encoded) for m in range(5)], axis=0)
