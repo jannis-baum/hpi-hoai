e4c9ca15a4b80c79447c1bc2f92d34117ff95ec2
Jannis Baum <jannis.baum@gmail.com>
2024-08-09 22:14:17 +0200

    feat: asymetric rmse

--- script/misc.py
+++ script/misc.py
@@ -1,6 +1,7 @@
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
 
 from definitions import fig_dir
 
@@ -10,3 +11,16 @@ def savefig_pdf(name):
     pdf_path = os.path.join(fig_dir, f'{name}.pdf')
 
     plt.savefig(pdf_path, bbox_inches='tight')
+
+# root mean squared with additional penalty for under estimation
+def asym_rmse(y_true, y_pred, under_estimation_penalty: float):
+    diff = y_true - y_pred
+    under_estimates = diff > 0
+
+    diff **= 2
+    # penalize under estimation
+    diff[under_estimates] *= under_estimation_penalty
+
+    # take penalty into account while normalizing to stay in same ball park
+    normalized_count = sum(under_estimates) * under_estimation_penalty + sum(~under_estimates)
+    return np.sqrt(diff.sum() / normalized_count)
